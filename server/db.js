13aecb9d99c583873ef492b25800f614b63e1d89
vetheve <victorien.etheve@outlook.fr>
2023-01-13 06:16:37 +0000

    "ADD: features function - data.json file - init ava"

--- server/db.js
+++ server/db.js
@@ -1,8 +1,36 @@
+const fs = require('fs'); // Importing the fs library
+
+/*FUNCTIONS*/
+
 // Function to retrieve all data from the JSON database 
 const getAllFromDatabase = (key) => {
-    // Parse the data from the JSON file
-    const data = JSON.parse(fs.readFileSync('./data.json', 'utf8'))
+    try {
+      // Read the data from the file and parse it as JSON
+      const data = JSON.parse(fs.readFileSync('./data.json', 'utf8'));
+  
+      // Check if the data is empty
+      if (!data) {
+        return null;
+      }
+  
+      // Return the data for the specified key
+      return data[key];
+    } catch (err) {
+      // Log the error message to the console
+      console.error(err);
+      // Return null if there is an error
+      return null;
+    }
+  };
 
-    // Return the data for the specified key, or null if the key does not exist
-    return data[key] || null;
-}
+
+/* MODULE EXPORTS*/
+
+module.exports = {
+    getAllFromDatabase,
+    getFromDatabaseById,
+    addToDatabase,
+    updateInstanceInDatabase,
+    deleteFromDatabasebyId,
+    deleteAllFromDatabase,
+  };
