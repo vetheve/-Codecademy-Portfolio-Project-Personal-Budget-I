eb65cc32d7de7d2a107f49536aa0c70e57e8ec3d
vetheve <victorien.etheve@outlook.fr>
2023-01-13 05:51:52 +0000

    "ADD : Add file data.json with the model"

--- test/test.js
+++ test/test.js
@@ -1,20 +1,13 @@
 const test = require("ava");  // Importing the AVA test library
 const request = require("supertest");  // Importing the supertest library for making HTTP requests
-const budgetsRouter = require('./budgets.js');  // Importing the router file for the '/budgets' route
-const expensesRouter = require('./expenses.js'); // Importing the router file for the '/expenses' route
-
-// Setting up the test context before each test run
-test.beforeEach(t => {
-    t.context.budgetsServer = budgetsRouter;
-    t.context.expensesServer = expensesRouter;
-});
+const apiRouter = require('./../server/api.js');  // Importing the api router file for the all the routers
 
 /*BUDGETS*/
 
 // Test to check if the GET request to '/budgets' route returns an array of all budgets
 test('GET /budgets should return an array of all budgets', async t => {
     // Making a GET request to the '/budgets' route
-    const res = await request(t.context.budgetsServer).get('/budgets');
+    const res = await request(apiRouter).get('/budgets');
 
     // Asserting that the status code of the response is 200
     t.is(res.status, 200);
@@ -27,7 +20,7 @@ test('GET /budgets should return an array of all budgets', async t => {
 // Test to check if the GET request to '/expenses' route returns an array of all expenses
 test('GET /expenses should return an array of all expenses', async t => {
     // Making a GET request to the '/budgets' route
-    const res = await request(t.context.expensesServer).get('/expenses');
+    const res = await request(apiRouter).get('/expenses');
 
     // Asserting that the status code of the response is 200
     t.is(res.status, 200);
@@ -40,7 +33,7 @@ test('GET /expenses should return an array of all expenses', async t => {
 // Test to check if the GET request to '/revenues' route returns an array of all revenues
 test('GET /expenses should return an array of all expenses', async t => {
     // Making a GET request to the '/budgets' route
-    const res = await request(t.context.expensesServer).get('/expenses');
+    const res = await request(apiRouter).get('/expenses');
 
     // Asserting that the status code of the response is 200
     t.is(res.status, 200);
@@ -48,5 +41,15 @@ test('GET /expenses should return an array of all expenses', async t => {
     t.true(Array.isArray(res.body));
 });
 
-/*REVENUES*/
+/*BALANCE*/
 
+// Test to check if the GET request to '/balance' route returns an array of all balances
+test('GET /balance should return an array of all balances', async t => {
+    // Making a GET request to the '/balance' route
+    const res = await request(apiRouter).get('/expenses');
+
+    // Asserting that the status code of the response is 200
+    t.is(res.status, 200);
+    // Asserting that the response body is an array
+    t.true(Array.isArray(res.body));
+});
