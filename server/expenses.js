28520735f964d6691536592492e5791622299820
vetheve <victorien.etheve@outlook.fr>
2023-01-24 10:47:06 +0000

    "ADD: Add response status codes for expense routes"

--- server/expenses.js
+++ server/expenses.js
@@ -29,19 +29,66 @@ module.exports = expensesRouter;
 expensesRouter
   .route('/')
   // Get all expenses
-  .get((req, res) => res.send(getAllFromDatabase('expenses')))
-  // Add a new expense to the list
-  .post((req, res) => {
-    addExpenseToDatabase(req.body.amount, req.body.description, req.body.budget_id, req.body.category)
-    res.status(201).send("Expense added successfully")
-  });
+    // Endpoint to get all expenses
+    .get((req, res) => {
+        const allExpenses = getAllFromDatabase('expenses');
+        if (allExpenses) {
+            res.status(200).send(allExpenses);
+        } else {
+            res.status(404).send({
+                error: "No expenses found"
+            });
+        }
+    })
+    // Add a new expenses to the list
+    .post((req, res) => {
+        const addedExpense = addExpenseToDatabase(req.body.amount, req.body.description, req.body.budget_id, req.body.category);
+        if (addedExpense) {
+            res.status(201).send(addedExpense);
+        } else {
+            res.status(400).send({
+                error: "Failed to add expense"
+            });
+        }
+    });
   
-// Endpoint to handle requests to a specific expense resource by ID
+// Endpoint to handle requests to a specific expenseresource by ID
 expensesRouter
-  .route('/:id')
-  // Get a specific expense by ID
-  .get((req, res) => res.send(getFromDatabaseById(req.params.id)))
-  // Update an existing expense in the list
-  .put((req, res) => res.send(updateInstanceInDatabase('expenses', req.params.id, req.body)))
-  // Delete a specific expense from the list
-  .delete((req, res) => res.sendStatus(deleteFromDatabasebyId(req.params.id)? 204 : 500));
+    .route('/:id')
+    // Get a specific expenseby ID
+    .get((req, res) => {
+        const getExpense = getFromDatabaseById(req.params.id)
+        if (getExpense) {
+            res.status(200).send(getExpense);
+        } else {
+            res.status(404).send({
+                error: "Expense not found"
+            });
+        }
+    })
+    // Update an existing expense in the list
+    .put((req, res) => {
+        const {
+            key,
+            value
+        } = req.body;
+        const updatedExpense = updateInstanceInDatabase(req.params.id, key, value, jsonData);
+        if (updatedExpense) {
+            res.status(200).send(updatedExpense);
+        } else {
+            res.status(404).send({
+                error: "Expense not found"
+            });
+        }
+    })
+    // Delete a specific expense from the list
+    .delete((req, res) => {
+        const deletedExpense = deleteFromDatabasebyId(req.params.id);
+        if (deletedBudget) {
+            res.status(200).send(deletedExpense);
+        } else {
+            res.status(404).send({
+                error: "Expense not found"
+            });
+        }
+    });
